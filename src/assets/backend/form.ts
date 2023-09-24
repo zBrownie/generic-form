8b47e5aee37dfe4f8bc83253a20b17694e39db61
Bruno Zenerato <bru.nomoraes@hotmail.com>
2023-09-23 21:30:16 -0300

    code

---
+++ src/assets/backend/form.ts
@@ -0,0 +1,74 @@
+import { FromTemplate } from "../../types/Form.types";
+
+export const formTemplate: FromTemplate = {
+  id: "form1-pep",
+  title: "Form to DEVS",
+  questions: [
+    {
+      id: "1",
+      type: "text",
+      label: "e-mail",
+      result: "bruno@email.com",
+      name: "email",
+    },
+    {
+      id: "2",
+      type: "radio",
+      label: "Question yes or no?",
+      name: "yerOrNo",
+      options: [
+        {
+          id: "1",
+          optionLabel: "Sim",
+          optionValue: "true",
+        },
+        {
+          id: "2",
+          optionLabel: "Não",
+          optionValue: "false",
+        },
+        {
+          id: "3",
+          optionLabel: "N/S",
+          optionValue: "unknow",
+        },
+      ],
+      result: "false",
+    },
+    {
+      id: "3",
+      type: "checkbox",
+      label: "Week Days",
+      name: "weekdays",
+      options: [
+        {
+          id: "1",
+          optionLabel: "Sunday",
+          optionValue: "sunday",
+        },
+        {
+          id: "2",
+          optionLabel: "Saturday",
+          optionValue: "saturday",
+        },
+        {
+          id: "3",
+          optionLabel: "Friday",
+          optionValue: "friday",
+        },
+      ],
+      result: [
+        {
+          id: "2",
+          optionLabel: "Saturday",
+          optionValue: "saturday",
+        },
+        {
+          id: "3",
+          optionLabel: "Friday",
+          optionValue: "friday",
+        },
+      ],
+    },
+  ],
+};
---
+++ src/types/Form.types.ts
@@ -0,0 +1,29 @@
+import { JSX } from "react";
+
+export interface FromTemplate {
+  id: string;
+  title: string;
+  questions: QuestionProps[];
+}
+
+export type QuestionTypes = "text" | "checkbox" | "radio" | "default";
+
+export interface QuestionProps {
+  id: string;
+  type: QuestionTypes;
+  label: string;
+  name: string;
+  result: string | QuestionOptionsprops[];
+  options?: QuestionOptionsprops[];
+}
+
+export interface QuestionOptionsprops {
+  id: string;
+  optionLabel: string;
+  optionValue: string;
+}
+
+export type QuestionComponentsProps = Record<
+  QuestionTypes,
+  (question: QuestionProps) => JSX.Element
+>;
